fb8565b8a19e4a6c69318c881679b6d7964cbac8
Jon Hall <code@jall.me>
2020-12-06 18:36:41 +0000

    Day 1:1

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
+import Data.Maybe (fromMaybe)
 import qualified Day_01
 
 main :: IO ()
-main = Day_01.solution
+main = putStrLn $ "Day 1: " ++ (fromMaybe "No solution found" Day_01.solution)
