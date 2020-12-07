05e3f37c51ef0855ea6414887e42b594063041b3
Jon Hall <code@jall.me>
2020-12-07 11:49:34 +0000

    Add unit tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,10 @@
-main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+import qualified Day_01
+import Test.Tasty
+import Test.Tasty.HUnit
+
+main = defaultMain tests
+
+tests :: TestTree
+tests = testGroup "Tests" [day1]
+
+day1 = testCase "Day 1:1" $ assertEqual "" Day_01.solution (Just "252724")
