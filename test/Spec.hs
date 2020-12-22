5a5bf8e6c8e61df94131d3b173aa627ab67a1150
Jon Hall <code@jall.me>
2020-12-22 12:57:20 +0000

    Fix test expectation/actual order

--- test/Spec.hs
+++ test/Spec.hs
@@ -16,22 +16,22 @@ main = defaultMain tests
 tests :: TestTree
 tests = testGroup "Tests" [day1, day2, day3, day4, day5, day6, day7, day8, day9, day10]
 
-day1 = testCase "Day 1" $ assertEqual "" Day_01.solution (Just "252724", Just "276912720")
+day1 = testCase "Day 1" $ assertEqual "" (Just "252724", Just "276912720") Day_01.solution
 
-day2 = testCase "Day 2" $ assertEqual "" Day_02.solution (Just "600", Just "245")
+day2 = testCase "Day 2" $ assertEqual "" (Just "600", Just "245") Day_02.solution
 
-day3 = testCase "Day 3" $ assertEqual "" Day_03.solution (Just "270", Just "2122848000")
+day3 = testCase "Day 3" $ assertEqual "" (Just "270", Just "2122848000") Day_03.solution
 
-day4 = testCase "Day 4" $ assertEqual "" Day_04.solution (Just "192", Just "101")
+day4 = testCase "Day 4" $ assertEqual "" (Just "192", Just "101") Day_04.solution
 
-day5 = testCase "Day 5" $ assertEqual "" Day_05.solution (Just "913", Just "717")
+day5 = testCase "Day 5" $ assertEqual "" (Just "913", Just "717") Day_05.solution
 
-day6 = testCase "Day 6" $ assertEqual "" Day_06.solution (Just "6443", Just "3232")
+day6 = testCase "Day 6" $ assertEqual "" (Just "6443", Just "3232") Day_06.solution
 
-day7 = testCase "Day 7" $ assertEqual "" Day_07.solution (Just "278", Just "45157")
+day7 = testCase "Day 7" $ assertEqual "" (Just "278", Just "45157") Day_07.solution
 
-day8 = testCase "Day 8" $ assertEqual "" Day_08.solution (Just "1179", Just "1089")
+day8 = testCase "Day 8" $ assertEqual "" (Just "1179", Just "1089") Day_08.solution
 
-day9 = testCase "Day 9" $ assertEqual "" Day_09.solution (Just "133015568", Just "16107959")
+day9 = testCase "Day 9" $ assertEqual "" (Just "133015568", Just "16107959") Day_09.solution
 
-day10 = testCase "Day 10" $ assertEqual "" Day_10.solution (Just "1690", Nothing)
+day10 = testCase "Day 10" $ assertEqual "" (Just "1690", Nothing) Day_10.solution
